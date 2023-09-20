4c2bc2cf33c67cef04d221ba7ebf05349d6048fa
Joseph Sourav Gomes <sourav.gomes@gmail.com>
2023-09-21 01:37:16 +0530

    comit

---
+++ Bike Store Project/SQLQuery3.sql
@@ -0,0 +1,15 @@
+select * from sales.orders
+
+select * from sales.customers
+
+select * from sales.order_items
+
+select * from production.products
+
+select * from production.categories
+
+select * from sales.stores
+
+select * from sales.staffs
+
+select * from production.brands
